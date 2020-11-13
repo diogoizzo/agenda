3960c86945a02ba71977a073a88e621f931c8db6
Diogo Izzo <izzoadv@gmail.com>
2020-11-13 18:02:46 -0300

    Impedir listagem de  contato sem login

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,13 @@
 const Contato = require('../models/ContatoModel');
 exports.index = async (req, res) => {
-  const contatos = await Contato.buscaContatos();
-  res.render('index', {contatos});
+  let logado = false;
+  if (req.session.user){
+    const contatos = await Contato.buscaContatos();
+    res.render('index', {contatos}, logado);
+  }else{
+    let contatos = {};
+    res.render('index', {contatos});
+  }
+  
 };
 
